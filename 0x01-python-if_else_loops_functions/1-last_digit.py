fa80b392039da37125b3cb8a23eb2763e95b0873
BinaryDev <eoalabi@student.oauife.edu.ng>
2024-03-17 21:52:42 +0100

    done

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import random
+
+number = random.randint(-10000, 10000)
+
+str_number = str(number)
+
+if int(str_number[-1]) > 5:
+    print(f"last digit of {number} is {str_number[-1]} and is greater than 5")
+elif int(str_number[-1]) == 0:
+    print(f"last digit of {number} is {str_number[-1]} and is 0")
+elif int(str_number[-1]) < 6 and str_number[-1] != 0:
+    print(f"last digit of {number} is {str_number[-1]} and is less than 6 and not 0")
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabet.py
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+#for i in range(26):
+ #   if i != 4 and i != 16:
+ #     print(chr(ord('a') + i))
+        
+for i in range(26):
+    if (ord('a') + i) != 101 and (ord('a') + i) != 113:
+        print(chr(ord('a') + i), end="")
