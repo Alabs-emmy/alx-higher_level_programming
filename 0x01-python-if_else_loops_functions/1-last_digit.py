4dc930effa7a436f581f9aaecd7d72f0fd8166ae
BinaryDev <eoalabi@student.oauife.edu.ng>
2024-03-18 16:23:13 +0100

    fixed

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,10 +8,10 @@ str_number = str(number)
 
 if int(str_number[-1]) > 5:
     print(f"last digit of {number} is {str_number[-1]} \
-            and is greater than 5")
+and is greater than 5")
 elif int(str_number[-1]) == 0:
     print(f"last digit of {number} is {str_number[-1]} \
-            and is 0")
+and is 0")
 elif int(str_number[-1]) < 6 and str_number[-1] != 0:
     print(f"last digit of {number} is {str_number[-1]} \
-             and is less than 6 and not 0")
+ and is less than 6 and not 0")
