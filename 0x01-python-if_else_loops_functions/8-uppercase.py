0efd99ec2ef9fa7078eb3066896ead9d44b253e4
BinaryDev <eoalabi@student.oauife.edu.ng>
2024-04-10 11:29:41 +0100

    convert string to upper case

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def uppercase(str):
+    """Prints string in uppercase"""
+
+    for i in str:
+        k = ord(i)
+        if (97 <= k <= 122):
+            k -= 32
+            i = chr(k)
+        print("{}".fornat(i), end="")
+    print()
