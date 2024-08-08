d141145696a79c13dd81a15949610eb1f494f551
Alabs-emmy <eoalabi@student.oauife.edu.ng>
2024-08-08 16:20:48 -0400

    spelling correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,11 +5,11 @@ if (__name__ == "__main__"):
     argc = len(sys.argv) - 1    # Arguements count
 
     if (argc == 0):
-        print("{} arguements.".format(argc))
+        print("{} arguments.".format(argc))
     else:
         if (argc == 1):
-            print("{} arguement:".format(argc))
+            print("{} argument:".format(argc))
         else:
-            print("{} arguements:".format(argc))
+            print("{} arguments:".format(argc))
         for i in range(1, argc + 1):
             print("{}: {}".format(i, sys.argv[i]))
