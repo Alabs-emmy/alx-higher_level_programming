22629e49309f1a427ad83f693f7dcfd0b119f8e8
Alabs-emmy <eoalabi@student.oauife.edu.ng>
2024-08-09 07:43:16 -0400

    iteration error

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -6,6 +6,6 @@ if (__name__ == "__main__"):
     List = dir(hidden_4)
 
     for i in List:
-        if (List[0] == '_'):
+        if (i[0] == '_'):
             continue
         print("{}".format(i))
