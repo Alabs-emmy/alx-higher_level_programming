d14d5480c4684128e98f06189839f34603bbfbf8
Alabs-emmy <eoalabi@student.oauife.edu.ng>
2024-08-09 08:37:55 -0400

    change dbracket

--- 0x02-python-import_modules/alphabet_upper.py
+++ 0x02-python-import_modules/alphabet_upper.py
@@ -2,7 +2,7 @@
 
 if (__name__ != "__main__"):
     for i in range(65, 91):
-        letter = chr[i]
+        letter = chr(i)
         if (letter != 90):
             print("{}".format(letter), end='')
         else:
