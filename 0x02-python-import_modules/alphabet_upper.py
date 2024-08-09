b9bf9e36c5387f9e034374b9483963fe37f85715
Alabs-emmy <eoalabi@student.oauife.edu.ng>
2024-08-09 08:43:45 -0400

    iteration variable corected

--- 0x02-python-import_modules/alphabet_upper.py
+++ 0x02-python-import_modules/alphabet_upper.py
@@ -3,7 +3,7 @@
 if (__name__ != "__main__"):
     for i in range(65, 91):
         letter = chr(i)
-        if (letter != 90):
+        if (i != 90):
             print("{}".format(letter), end='')
         else:
             print("{}".format(letter))
