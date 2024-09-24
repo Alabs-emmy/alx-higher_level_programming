44b51315d21dfb332936f486f807a0bbd24a445a
Alabs-emmy <eoalabi@student.oauife.edu.ng>
2024-09-24 02:02:38 -0400

    Operand type corrected

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,7 +6,7 @@ def print_matrix_integer(matrix=[[]]):
         if (len(matrix[i]) == 0):
             print()
         for j in range(0, len(matrix[i])):
-            if (j != len(matrix[i] - 1)):
+            if (j != len(matrix[i]) - 1):
                 print("{:d}".format(matrix[i][j], end=' '))
-            elif (j == len(matrix[i] - 1)):
+            elif (j == len(matrix[i]) - 1):
                 print("{:d}".format(matrix[i][j]))
